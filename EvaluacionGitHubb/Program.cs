e6eae3642827621d2ce6d0ffe7663bd346d31ed0
Dennispolanc <denispolanco2023@gmail.com>
2025-05-13 14:28:07 -0600

    Update Program.cs

--- EvaluacionGitHubb/Program.cs
+++ EvaluacionGitHubb/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Proyecto de Ventas");
-            "Primer commint del Proyecto"
+            "Primer commint del Proyecto";
         }
     }
 }
